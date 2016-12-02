1f7b5805a8ca75a2da91d11974c58250b90c193a
Donald Jackson <donald.jackson@bms.com>
2016-12-02 18:34:31 -0500

    Added customizable base_theme

--- R/theme_cornerLabels.R
+++ R/theme_cornerLabels.R
@@ -1,9 +1,11 @@
-# theme_cornerLabels
-# modification to theme_dj that left-justifies text
-# and removes strip background
+#' Modified theme for corner labels
+#' 
+#' Removes facet strip background and left-justifies labels
 
-theme_cornerLabels <- function(base_size = 12, base_family = "") {
-  theme_dj(base_size = base_size, base_family = base_family) %+replace%
+theme_cornerLabels <- function(base_size = 12, base_family = "", 
+                               base_theme = theme_bw()) {
+  theme_dj(base_size = base_size, base_family = base_family,
+           base_theme = base_theme) +
     theme(strip.text = element_text(hjust = 0),
           strip.background = element_blank(),
           strip.placement = "outside")
