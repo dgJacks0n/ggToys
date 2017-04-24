d8ae24b0f92f356fbe6d4c2078d095e637a51d34
Donald Jackson <donald.jackson@bms.com>
2017-04-24 08:45:43 -0400

    Add lut5fill function; split defaults into separate function for consistency

--- R/lut5color.R
+++ R/lut5color.R
@@ -1,4 +1,19 @@
+#' 5-color pseudocolor lookup
+#' Color gradient for points, lines, etc
+#'
+lut5 <- function() {
 
-lut5color <- function( colors = c("#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000") ) {
-                       lut5color <- ggplot2::scale_color_gradientn(colors = colors)
+  mycolors <- c("#0000FF", "#00FF00", "#FFFF00",
+           "#FF7F00", "#FF0000")
+  return(mycolors)
 }
+
+  lut5color <- function( colors = lut5() ) {
+    return(ggplot2::scale_color_gradientn(colors = colors))
+  }
+
+
+lut5fill <- function( colors = lut5() ) {
+  return(ggplot2::scale_fill_gradientn(colors = colors))
+}
+
