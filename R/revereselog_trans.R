671134b41f1e04055c0e9a2ba6a628584cf0addc
DGJackson <14615921+dgJacks0n@users.noreply.github.com>
2020-04-09 17:14:11 -0400

    Updated examples, used named arguments to scales::trans_new()

--- R/revereselog_trans.R
+++ R/revereselog_trans.R
@@ -1,17 +1,27 @@
 #' Reverse log transform for values < 1
-#' @examples
-#'   myplot <- myplot + scale_y_continuous(trans=reverselog_trans(base = 2)) 
-#'   
 #' @param base (default 10): base for log tranformation
+#' 
+#' @examples
+#' \dontrun{
+#'   # add reverse log tranformation to ggplot2 plot
+#'   myplot <- myplot + scale_y_continuous(trans = reverselog_trans())
+#'   
+#'   # change transformation to base 2
+#'   myplot <- myplot + scale_y_continuous(trans=reverselog_trans(2)) 
+#' }
 #' 
 #' @export
 #' 
 
 
 reverselog_trans <- function(base = 10) {
-  trans <- function(x) -log(x, base)
-  inv <- function(x) base^(-x)
-  scales::trans_new(paste0("reverselog-", format(base)), trans, inv, 
-            scales::log_breaks(base = base), 
-            domain = c(1e-100, Inf))
+  trans <- function(x) { (-1 * log(x, base))}
+  
+  inv <- function(x) { base^(-1 * x) }
+  
+  scales::trans_new(name = paste0("reverselog-", format(base)), 
+                    transform = trans, 
+                    inverse = inv, 
+                    breaks = scales::breaks_log(base = base), 
+                    domain = c(1e-100, Inf))
 }
